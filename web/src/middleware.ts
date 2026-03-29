f3f29be26064938d41d24b3db55b34d214524065
Aviv Oron <avivoron@gmail.com>
2026-03-29 16:28:46 +0300

    bug fix

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,6 +1,9 @@
 import { withAuth } from 'next-auth/middleware'
 
-const authMiddleware = withAuth({ pages: { signIn: '/' } })
+const authMiddleware = withAuth({
+  pages: { signIn: '/' },
+  secret: process.env.NEXTAUTH_SECRET
+})
 
 export default authMiddleware
 
