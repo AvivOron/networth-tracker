3cbd6b181c98c34b67b7c3184667446e28ad5eee
Aviv Oron <avivoron@gmail.com>
2026-04-01 14:19:12 +0300

    revert change

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <Providers>{children}</Providers>
-        <Analytics scriptSrc="https://va.vercel-scripts.com/v1/script.js" endpoint="https://vitals.vercel-insights.com/v1/vitals" />
+        <Analytics />
       </body>
     </html>
   )
