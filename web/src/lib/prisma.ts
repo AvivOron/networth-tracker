3aea745308470bf2e559ef0c3ef3e235bc20bddd
Aviv Oron <avivoron@gmail.com>
2026-04-13 21:42:48 +0300

    move to neon

--- web/src/lib/prisma.ts
+++ web/src/lib/prisma.ts
@@ -1,14 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = globalThis as unknown as { prisma: ReturnType<typeof makePrisma> }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-function makePrisma() {
-  return new PrismaClient({
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error']
-  }).$extends(withAccelerate())
-}
-
-export const prisma = globalForPrisma.prisma ?? makePrisma()
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
